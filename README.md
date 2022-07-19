dbd1ff71c769bb4e40798a6a5a6a65d5583c955d
cl <chencassc@gmail.com>
2022-07-19 11:34:02 +0800

    Play (#1)
    * init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# xcarnival-test
-xcarnival hack reproduced
+# A Hardhat Project to reproduce the xcarinaval hack
+
+A demo reproducing the [xcarnival hack](https://xcarnival-lab.medium.com/xcarnival-has-got-1-467-eth-back-the-security-agencies-have-tentatively-determined-the-hackers-3ea05ad134ae) using hardhat fork.
+
+To run the demo:
+
+```shell
+npx hardhat run scripts/xcarnival-play.js
+```
---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {version: "0.8.9"},
+      {version: "0.8.2"},
+    ],
+  },
+  mocha: {
+    timeout: 400000000
+  },  
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://mainnet.infura.io/v3/36fd3e30e77e47ba9034d2605a11ec8a",
+        blockNumber: 15028719,
+      }
+    }
+  },
+};
